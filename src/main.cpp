d960a6660bddaf4db304757d846d6e4ffd47e2b0
TomiLimoni <136496933+TomiLimoni@users.noreply.github.com>
2026-03-28 17:15:09 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 // Copyright 2022 UNN-IASR
+#include <iostream>
 #include "fun.h"
 
 int main() {
-    return 0;
+    const char* str1 = "Hello World 1234    ab2c def234gh 56ij78k 0    x 1ab abc12";
+    const char* str2 = "Hello world aBc abC ABcd abCd R ";
+    const char* str3 = "";
+    const char* str4 = "   ";
+    std::cout << str1 << " | count = " << faStr1(str1) << "\n";
+    std::cout << str1 << " | average length = " << faStr3(str1) << "\n";
+    std::cout << str2 << " | count = " << faStr2(str2) << "\n";
+    std::cout << str2 << " | average length = " << faStr3(str2) << "\n";
+    std::cout << str3 << " | count = " << faStr1(str3) << "\n";
+    std::cout << str3 << " | average length = " << faStr3(str3) << "\n";
+    std::cout << str4 << " | count = " << faStr2(str4) << "\n";
+    std::cout << str4 << " | average length = " << faStr3(str4) << "\n";
 }
